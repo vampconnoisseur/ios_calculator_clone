fc8f6eb59eb060bc6cec7b6cafad250476ff0995
Jaiditya Batra <jaiditya7318@gmail.com>
2023-06-21 16:13:45 +0530

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Jaiditya Batra on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
